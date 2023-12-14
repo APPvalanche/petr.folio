ee9710276c2d3d1d0897b2e46f20d0d5815e2d97
Petr Zajkas <petrzajkas@gmail.com>
2023-12-14 21:04:47 +0000

    navbar and hero section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 
-import { AppWrap } from '../../wrapper';
+import { AppWrap } from "../../wrapper";
 import { images } from "../../constants";
 import "./Header.scss";
 
@@ -23,48 +23,40 @@ const Header = () => {
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
         className="app__header-info"
-        >
+      >
         <div className="app__header-badge">
           <div className="badge-cmp app__flex">
-            
             <div style={{ marginLeft: 0 }}>
               <h2 className="head-text">Hi, I am Petr.</h2>
             </div>
           </div>
           <div className="tag-cmp app__flex">
-            <p className="p-text">Web Developer  |  Freelancer</p>
+            <p className="p-text">Web Developer | Freelancer</p>
           </div>
         </div>
       </motion.div>
-      
+
       <motion.div
         whileInView={{ opacity: [0, 1] }}
         transition={{ duration: 0.5, delayChildren: 0.5 }}
         className="app__header-img"
-        >
+      >
         <img src={images.profile} alt="profile_bg" />
-        <motion.img
-          whileInView={{ scale: [0, 1] }}
-          transition={{ duration: 1, ease: "easeInOut" }}
-          src={images.circle}
-          alt="profile_circle"
-          className="overlay_circle"
-        />
       </motion.div>
 
       <motion.div
         variant={scaleVariants}
         whileInView={scaleVariants.whileInView}
         className="app__header-circles"
-        >
+      >
         {[images.node, images.react, images.sass].map((circle, index) => (
           <div className="circle-cmp app__flex" key={`circle-${index}`}>
             <img src={circle} alt="circle" />
           </div>
         ))}
       </motion.div>
-      </div>
+    </div>
   );
 };
 
-export default AppWrap(Header, 'home');
+export default AppWrap(Header, "home");
