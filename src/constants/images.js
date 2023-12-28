1fa7ce47cef9a81b0be8701eb64c760f67754893
Petr Zajkas <petrzajkas@gmail.com>
2023-12-28 15:29:10 +0000

    bento grid

--- src/constants/images.js
+++ src/constants/images.js
@@ -16,6 +16,7 @@ import redux from "../assets/redux.png";
 import sass from "../assets/sass.png";
 import typescript from "../assets/typescript.png";
 import vue from "../assets/vue.png";
+import gradient_background from "../assets/gradient_background.jpg";
 
 import about01 from "../assets/about01.png";
 import about02 from "../assets/about02.png";
@@ -55,4 +56,5 @@ export default {
   circle,
   logo,
   amazon,
+  gradient_background,
 };
---
+++ src/container/Bento/Bento.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Bento.scss';
+import { AppWrap, MotionWrap } from '../../wrapper';
+
+const Bento = () => {
+  return (
+
+
+    <div id="bento">
+        <div className='app__social'>
+
+        </div>   
+
+        <div className='wrapper'>
+          <div className="grid1 glassmorphism__dark">1</div>
+          <div className="grid2 glassmorphism__dark">2</div>
+          <div className="grid3 glassmorphism__dark">3</div>
+          <div className="grid4 glassmorphism__dark">4</div>
+          <div className="grid5 glassmorphism__dark">5</div>
+          <div className="grid6 glassmorphism__dark">6</div>
+          <div className="grid7 glassmorphism__dark">7</div>
+          <div className="grid8 glassmorphism__dark">8</div>
+          <div className="grid9 glassmorphism__dark">9</div>
+          <div className="grid10 glassmorphism__dark">10</div>
+          <div className="grid11 glassmorphism__dark">11</div>
+        </div>
+
+        <div className='app__navigation'>
+
+        </div>
+   </div>
+  )
+}
+
+export default Bento;
