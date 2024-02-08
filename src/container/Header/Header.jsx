78179aa0e0ff3d88b4b17abea04aa9decd842e9d
Petr Zajkas <petrzajkas@gmail.com>
2024-02-08 18:18:56 +0000

    final commit

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
 
-
 import { AppWrap, MotionWrap } from "../../wrapper";
 import { images } from "../../constants";
 import { BsArrowDownRightCircle } from "react-icons/bs";
@@ -10,42 +9,39 @@ import { BsArrowDownRightCircle } from "react-icons/bs";
 import "./Header.scss";
 
 const Header = () => {
-
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
     <>
-    <div className="app__header app__flex">
-      <motion.div
-        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-        transition={{ duration: 0.5 }}
-        className="app__header-description"
-      >
-        <div className="description">
-          <h2>{t("greeting")}</h2>
-          <p className="p-text">
-          {t("header_info")}
-          </p>
-          <div className="btns">
-            <button className="btn_contact">
-              <a href="#contact">{t("header_info_btn_contact")}</a>
-            </button>
-            <button className="btn_work">
-              <a href="#work">{t("header_info_btn_work")}</a>
-              <BsArrowDownRightCircle />
-            </button>
+      <div className="app__header app__flex">
+        <motion.div
+          whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+          transition={{ duration: 0.5 }}
+          className="app__header-description"
+        >
+          <div className="description">
+            <h2>{t("greeting")}</h2>
+            <p className="p-text">{t("header_info")}</p>
+            <div className="btns">
+              <button className="btn_contact">
+                <a href="#contact">{t("header_info_btn_contact")}</a>
+              </button>
+              <button className="btn_work">
+                <a href="#work">{t("header_info_btn_work")}</a>
+                <BsArrowDownRightCircle />
+              </button>
+            </div>
           </div>
-          </div>
-      </motion.div>
+        </motion.div>
 
-      <motion.div
-        whileInView={{ opacity: [0, 1] }}
-        transition={{ duration: 0.5, delayChildren: 0.5 }}
-        className="app__header-img"
-      >
-        <img src={images.profile} alt="profile_bg" />
-      </motion.div>
-    </div>
+        <motion.div
+          whileInView={{ opacity: [0, 1] }}
+          transition={{ duration: 0.5, delayChildren: 0.5 }}
+          className="app__header-img"
+        >
+          <img src={images.profile} alt="profile_bg" />
+        </motion.div>
+      </div>
     </>
   );
 };
@@ -53,5 +49,5 @@ const Header = () => {
 export default AppWrap(
   MotionWrap(Header, "app__header"),
   "home",
-  "app__primarybg",
+  "app__primarybg"
 );
