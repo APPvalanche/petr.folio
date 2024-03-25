e0a05a64fd602de0c376d9a54f0938469e259c71
Petr Zajkas <petrzajkas@gmail.com>
2024-03-25 16:15:03 +0000

    site information

--- src/container/Features/Features.jsx
+++ src/container/Features/Features.jsx
@@ -7,26 +7,28 @@ import { useTranslation } from "react-i18next";
 import "./Features.scss";
 
 const Features = () => {
-
-  const { t } = useTranslation()
+  const { t } = useTranslation();
 
   return (
     <>
-    <div className="app__features">
-      <div className="features">
-        <div className="features__animation">
-          <Lottie animationData={animationData} />
+      <div className="app__features">
+        <div className="features">
+          <div className="features__animation">
+            <Lottie animationData={animationData} />
+          </div>
+
+          <div className="features__text">
+            <h2 className="head-text">{t("feature_heading")}</h2>
+            <button>
+              <a
+                href="https://koalendar.com/e/meet-with-petr-zajkas-web-designer"
+                target="blank"
+              >
+                {t("feature_button")}
+              </a>
+            </button>
+          </div>
         </div>
-
-
-        <div className="features__text">
-          <h2 className="head-text">{t("feature_heading")}</h2>
-          <button>
-            <a href="#contact">{t("feature_button")}</a>
-          </button>
-
-        </div>
-      </div>
       </div>
     </>
   );
